bb865f28c1bfc0719aa22aad4ccb8bccbd4d5eff
Antoine PAINCHAUX <antoine.painchaux@edu.ece.fr>
2025-09-07 15:54:36 +0200

    Initial commit - Phase 0 blinky LED

---
+++ Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ /Users/antoinepainchaux/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.3/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ /Users/antoinepainchaux/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.3/Drivers/CMSIS/Include/core_cm4.h \
+ /Users/antoinepainchaux/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.3/Drivers/CMSIS/Include/cmsis_version.h \
+ /Users/antoinepainchaux/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.3/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /Users/antoinepainchaux/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.3/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /Users/antoinepainchaux/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.3/Drivers/CMSIS/Include/mpu_armv7.h \
+ /Users/antoinepainchaux/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.3/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+/Users/antoinepainchaux/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.3/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+/Users/antoinepainchaux/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.3/Drivers/CMSIS/Include/core_cm4.h:
+/Users/antoinepainchaux/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.3/Drivers/CMSIS/Include/cmsis_version.h:
+/Users/antoinepainchaux/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.3/Drivers/CMSIS/Include/cmsis_compiler.h:
+/Users/antoinepainchaux/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.3/Drivers/CMSIS/Include/cmsis_gcc.h:
+/Users/antoinepainchaux/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.3/Drivers/CMSIS/Include/mpu_armv7.h:
+/Users/antoinepainchaux/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.3/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
